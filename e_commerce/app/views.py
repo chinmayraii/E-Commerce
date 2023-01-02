946c5f64725aa66b9f344f60c8ecd87de595a988
CHINMAY RAI <114671850+chinmayraii@users.noreply.github.com>
2023-01-02 23:07:35 +0530

    add to cart

--- e_commerce/app/views.py
+++ e_commerce/app/views.py
@@ -44,7 +44,50 @@ def add_to_cart(request):
     return redirect('/cart')
 
 def show_cart(request):
-    return render(request,('app/addtocart.html'))
+    if request.user.is_authenticated:
+        user=request.user
+        cart= Cart.objects.filter(user=user)
+        amount = 0.0
+        shipping_amount=70.0
+        total_amount=0.0
+        cart_product=[p for p in Cart.objects.all() if p.user == user]
+        print(cart_product)
+        if cart_product:
+            for p in cart_product:
+                tempamount=(p.quantity*p.product.discounted_price)
+                amount+= tempamount
+                total_amount=amount+shipping_amount
+            return render(request, 'app/addtocart.html',{'carts':cart, 'total_amount':total_amount, 'amount':amount})
+        else:
+            return render(request, ('app/emptycart.html'))
+           
+
+# def plus_cart(request):
+#     if request.method == 'GET':
+#         prod_id= request.GET['prod_id']
+#         c = Cart.objects.get(Q(product=prod_id) & Q(user=request.user))
+#         c.quantity+=1
+#         c.save()
+#         amount =0.0
+#         shipping_amount=70.0
+#         cart_product = [p for p in Cart.objects.all() if p.user == request.user]
+#         for p in cart_product:
+#             tempamount=(p.quantity * p.product.discounted_price)
+#             amount+= tempamount
+            
+
+#         data ={
+#             'quantity': c.quantity,
+#             'amount': amount,
+#             'totalamount': amount + shipping_amount
+#             }
+#         return JsonResponse(data)  
+
+
+
+# def emptycart(request):
+#     return render(request, 'app/emptycart.html')
+
 
 def product_detail(request):
  return render(request, 'app/productdetail.html')
